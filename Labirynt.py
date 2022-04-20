65b4f6e4b18b45c55bd82c4dab1a61ea0f175c59
Dominik Szczepaniak <dominik@macbook-air-dominik.home>
2022-04-20 16:54:46 +0200

    Blokada wejscie jesli w neodpowiednim formacie

--- Labirynt.py
+++ Labirynt.py
@@ -1,6 +1,7 @@
+from email import message
 import tkinter as tk
 from collections import defaultdict
-# from tkinter import ttk
+from tkinter import messagebox
 from PIL import Image
 
 class SpanningTree:
@@ -23,6 +24,19 @@ class SpanningTree:
         for i in self.adj:
             print(i)
 
+def sprawdz(height, width, DataInput):
+    try:
+        height = int(height)
+        width = int(width)
+    except:
+        messagebox.showerror("Błąd", "Wartość powinna być z przedziału od 100 do 1000! Popraw wejście")
+        return
+    if(height < 100 or height > 1000 or width < 100 or width > 1000):
+        messagebox.showerror("Błąd", "Wartość powinna być z przedziału od 100 do 1000! Popraw wejście")
+        return
+    else:
+        DataInput.quit()
+
 def getSize():
     DataInput = tk.Tk()
     width_var = tk.StringVar()
@@ -31,7 +45,7 @@ def getSize():
     tk.Label(DataInput, text="Width:").grid(column=0, row=1)
     tk.Entry(DataInput, textvariable=height_var).grid(column=1, row=0)
     tk.Entry(DataInput, textvariable=width_var).grid(column=1, row=1)
-    tk.Button(DataInput, text="Generate", command=DataInput.quit).grid(column=0, row=3)
+    tk.Button(DataInput, text="Generate", command=lambda: sprawdz(height_var.get(), width_var.get(), DataInput)).grid(column=0, row=3)
     DataInput.mainloop()
     height = height_var.get()
     width = width_var.get()
@@ -42,7 +56,7 @@ def generate(width, height):
     Lab = tk.Tk()
     Lab.geometry("{0}x{1}".format(width, height))
     Lab.title("Labirynt gotowy!")
-    
+    Lab.resizable(False, False)
     Lab.mainloop()
 
 def main():
