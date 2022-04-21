c2b3a60ee4a25bce7a36d10ad785f934d89c4b1f
Dominik Szczepaniak <dominik@macbook-air-dominik.home>
2022-04-21 21:57:07 +0200

    Przeniesienie labiryntu do klasy

--- Labirynt.py
+++ Labirynt.py
@@ -27,44 +27,48 @@ class SpanningTree:
         for i in self.adj:
             print(i)
 
-def sprawdz(height, width, DataInput):
-    try:
-        height = int(height)
-        width = int(width)
-    except:
-        messagebox.showerror("Błąd", "Wartość powinna być z przedziału od 100 do 1000! Popraw wejście")
-        return
-    if(height < 100 or height > 1000 or width < 100 or width > 1000):
-        messagebox.showerror("Błąd", "Wartość powinna być z przedziału od 100 do 1000! Popraw wejście")
-        return
-    else:
-        DataInput.quit()
-
-def getSize():
-    DataInput = tk.Tk()
-    width_var = tk.StringVar()
-    height_var = tk.StringVar()
-    tk.Label(DataInput, text="Height:").grid(column=0, row=0)
-    tk.Label(DataInput, text="Width:").grid(column=0, row=1)
-    tk.Entry(DataInput, textvariable=height_var).grid(column=1, row=0)
-    tk.Entry(DataInput, textvariable=width_var).grid(column=1, row=1)
-    tk.Button(DataInput, text="Generate", command=lambda: sprawdz(height_var.get(), width_var.get(), DataInput)).grid(column=0, row=3)
-    DataInput.mainloop()
-    height = height_var.get()
-    width = width_var.get()
-    DataInput.destroy()
-    return width, height
-
-def generate(width, height):
-    Lab = tk.Tk()
-    Lab.geometry("{0}x{1}".format(width, height))
-    Lab.title("Labirynt gotowy!")
-    Lab.resizable(False, False)
-    Lab.mainloop()
+class Labirynth:
+    def __init__(self):
+        self.SizesGet()
+    def SizesGet(self):
+        DataInput = tk.Tk()
+        width_var = tk.StringVar()
+        height_var = tk.StringVar()
+        tk.Label(DataInput, text="Height:").grid(column=0, row=0)
+        tk.Label(DataInput, text="Width:").grid(column=0, row=1)
+        tk.Entry(DataInput, textvariable=height_var).grid(column=1, row=0)
+        tk.Entry(DataInput, textvariable=width_var).grid(column=1, row=1)
+        tk.Button(DataInput, text="Generate", command=lambda: self.checkSizes(height_var.get(), width_var.get(), DataInput)).grid(column=0, row=3)
+        DataInput.mainloop()
+        height = height_var.get()
+        width = width_var.get()
+        DataInput.destroy()
+        return self.generate(width, height)
+    def checkSizes(self, height, width, DataInput):
+        try:
+            height = int(height)
+            width = int(width)
+        except:
+            messagebox.showerror("Błąd", "Wartość powinna być z przedziału od 100 do 1000! Popraw wejście")
+            return
+        if(height < 100 or height > 1000 or width < 100 or width > 1000):
+            messagebox.showerror("Błąd", "Wartość powinna być z przedziału od 100 do 1000! Popraw wejście")
+            return
+        else:
+            DataInput.quit()
+    def generate(self, width, height):
+        Lab = tk.Tk()
+        Lab.geometry("{0}x{1}".format(width, height))
+        Lab.title("Labirynt gotowy!")
+        Lab.resizable(False, False)
+        Lab.mainloop()
+    def AskAgain(self):
+        pass
+        #funkcja pytajaca czy uzytkownik chce utworzyc labirynt jeszcze raz na podstawie innych wymiarow (pozniej mozna zmienic to, ze w jednym oknie bedzie wszystko dzialac i przy kliknieciu generate bedzie tworzyc nowy labirynt)
+    
 
 def main():
-    width, height = getSize()
-    generate(width, height)
+    newLab = Labirynth()
 
 if(__name__ == "__main__"):
     main()
