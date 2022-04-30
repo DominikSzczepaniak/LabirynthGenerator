4978d04b905468f08da988843b9afb84b51d7097
slxshxr17 <szczepaniakdominik5@gmail.com>
2022-04-30 16:33:11 +0200

    Created path nodelist

--- Labirynt.py
+++ Labirynt.py
@@ -18,7 +18,6 @@ import time
 
 class SpanningTree:
     def __init__(self, width, height):
-        self.adj = defaultdict(list)
         rozmiar = width * height
         self.parent = [i for i in range(rozmiar+5)]
         self.size = [1] * (rozmiar+5)
@@ -36,8 +35,6 @@ class SpanningTree:
                 a, b = b, a
             self.parent[b] = a
             self.size[a] += self.size[b]
-            self.adj[a].append(b)
-            self.adj[b].append(a)
 
 class Labirynth:
     def __init__(self):
@@ -53,6 +50,7 @@ class Labirynth:
         tk.Entry(DataInput, textvariable=width_var).grid(column=1, row=1)
         tk.Button(DataInput, text="Generate", command=lambda: self.checkSizes(height_var.get(), width_var.get(), DataInput)).grid(column=0, row=3)
         tkinter.Scale(DataInput, from_=4, to=64,variable=cellSize_var, orient=tkinter.HORIZONTAL).grid(column=0, row=4)
+        DataInput.geometry("400x200")
         DataInput.mainloop()
         cellSize = int(cellSize_var.get())
         if(cellSize%2==1):
@@ -84,11 +82,11 @@ class Labirynth:
         PhotoObject.pack()
         Lab.mainloop()
     def AskAgain(self):
-        pass
         #funkcja pytajaca czy uzytkownik chce utworzyc labirynt jeszcze raz na podstawie innych wymiarow (pozniej mozna zmienic to, ze w jednym oknie bedzie wszystko dzialac i przy kliknieciu generate bedzie tworzyc nowy labirynt)
-    def GenerateLabirynt(self, width, height, cellSize):
-        edges = []
-        vert = [[1, 0], [-1, 0], [0, 1], [0,-1]]
+        pass
+    def RandomiseEdges(self, width, height):
+        edgesForMap = [] #list of edges to generate map for labirynth
+        vert = [[1, 0], [-1, 0], [0, 1], [0,-1]] 
         for i in range(1, width*height+1):
             line = (i-1)//width+1 #getting line and column in map for node i
             column = i%width
@@ -104,15 +102,47 @@ class Labirynth:
                 node = (y-1)*width + x #converting map place to node number
                 if(node == i):
                     continue
-                edges.append([int(i), int(node), int(wartosc)])
-        edges.sort(key = lambda edges:edges[2])
+                edgesForMap.append([int(i), int(node), int(wartosc)])
+        edgesForMap.sort(key = lambda edges:edges[2])
+        return edgesForMap
+    def GeneratePath(self, width, height, adj):
+        q = []
+        visited = [False] * int(height*width+5)
+        odl = [1000000] * int(height*width+5)
+        q.append(1)
+        visited[1] = True
+        odl[1] = 0
+        while(len(q) > 0):
+            node = q.pop(0)
+            for u in adj[node]:
+                if(visited[u] == False):
+                    odl[u] = odl[node]+1
+                    visited[u] = True
+                    q.append(u)
+        pathLength = odl[width*height]
+        CurrentNode = width*height
+        path = []
+        while(pathLength > 0):
+            path.append(CurrentNode)
+            for u in adj[CurrentNode]:
+                if(odl[u] == pathLength-1):
+                    CurrentNode = u 
+                    pathLength = odl[u]
+        path.append(1)
+        path.reverse()
+        return path
+    def GenerateLabirynt(self, width, height, cellSize):
+        edges = self.RandomiseEdges(width, height)
         MSP = SpanningTree(width, height)
         polaczone = []
+        adj = defaultdict(list)
         for a, b, c in edges:
             if(MSP.FindSet(a) == MSP.FindSet(b)):
                 continue
             MSP.Union(a, b)
             polaczone.append([a, b])
+            adj[a].append(b)
+            adj[b].append(a)
         #for every node create info as :[0,0,0,0] meaning that there is no wall on the left, above, right, below, if number is 1, there is a wall. originally set every value to 1 and when there is edge in MSP make it 0. 
         Mapinfo = [[1,1,1,1] for i in range(width*height+5)] 
         for i in range(1, width+1): #taking care of above wall for every upmost cell
@@ -147,12 +177,14 @@ class Labirynth:
         id = 1
         for j in range(1, height+1):
             for i in range(1,width+1):
-                self.DrawCell(LabImDraw, i, j, Mapinfo[id], cellSize)
+                self.DrawWalls(LabImDraw, i, j, Mapinfo[id], cellSize)
                 id+=1 
         LabImDraw.rectangle([(0, 1), (0, cellSize-1)], fill = 'white', outline = 'white')
         LabImDraw.rectangle([(width*cellSize, 1+(height-1)*cellSize), (width*cellSize, height*cellSize-1)], fill = 'white', outline = 'white')
+        path = self.GeneratePath(width, height, adj)
+        #by hand do line to middle of cell 1 and last cell and write algorithm to connect other cells based on path nodelist
         return LabIm
-    def DrawCell(self, LabImDraw, x, y, infoDraw, cellSize):
+    def DrawWalls(self, LabImDraw, x, y, infoDraw, cellSize):
         # center = [[int(2+(x-1)*4)],[int(2+(y-1)*4)]]
         centerx = int(cellSize//2+(x-1)*cellSize)
         centery = int(cellSize//2+(y-1)*cellSize)
@@ -168,7 +200,12 @@ class Labirynth:
             LabImDraw.rectangle([prawygornyrog, prawydolnyrog], fill = 'black')
         if(infoDraw[3] == 1):
             LabImDraw.rectangle([lewydolnyrog, prawydolnyrog], fill = 'black')
+    def DrawPath(self, LabImDraw, x, y, whereFrom, whereTo, cellsize):
+        #whereFrom - info where did u come from (left, above, right, below)
+        #whereTo - info where are u going next (left, above, right, below)
+        #both these informations are used to color cells accordingly 
 
+        pass
 
 
 
